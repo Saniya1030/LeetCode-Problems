cb14bd43e4b1b12102c5ce3e0147b44b104f73aa
Saniya Sirtaz <saniyasirtaz1030@gmail.com>
2026-08-02 17:02:36 +0530

    Time: 4 ms (15.68%), Space: 48.5 MB (23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,42 +1,43 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>>adjls=new ArrayList<>();
         int n=isConnected.length;
-        int count=0;
+        ArrayList<ArrayList<Integer>>adjls=new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             adjls.add(new ArrayList<>());
         }
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<isConnected[0].length;j++)
             {
-                if(isConnected[i][j]==1 && i!=j)
+                if(isConnected[i][j]==1  && i!=j)
                 {
                     adjls.get(i).add(j);
                     adjls.get(j).add(i);
                 }
             }
         }
+
         boolean visited[]=new boolean[n];
+        int count=0;  
         for(int i=0;i<n;i++)
         {
             if(visited[i]==false)
             {
                 count++;
-                dfs(i,adjls,visited);
+                dfs(i,visited,adjls);
             }
         }
         return count;
     }
-    private void dfs(int start,ArrayList<ArrayList<Integer>>adjls,boolean []visited)
+    private void dfs(int start,boolean visited[],ArrayList<ArrayList<Integer>>adjls)
     {
         visited[start]=true;
-        for(int i:adjls.get(start))
+        for(int it:adjls.get(start))
         {
-            if(visited[i]!=true)
+            if(visited[it]!=true)
             {
-                dfs(i,adjls,visited);
+                dfs(it,visited,adjls);
             }
         }
     }
